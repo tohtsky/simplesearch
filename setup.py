f83d784beb5a998c80c9c361252b4e6691766be3
Tomoki Ohtsuki <tomoki.ohtsuki129@gmail.com>
2020-12-10 06:49:50 +0000

    more handy setup.py

--- setup.py
+++ setup.py
@@ -1,50 +1,125 @@
-from setuptools import setup
-from glob import glob
-
-# Available at setup time due to pyproject.toml
-from pybind11.setup_helpers import Pybind11Extension, build_ext
-from pybind11 import get_cmake_dir
-
+from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext
+import sys
+import setuptools
+import os
 
 __version__ = "0.0.1"
+install_requires = ["pybind11>=2.5", "numpy >= 1.11"]
 
-# The main interface is through Pybind11Extension.
-# * You can add cxx_std=11/14/17, and then build_ext can be removed.
-# * You can set include_pybind11=false to add the include directory yourself,
-#   say from a submodule.
-#
-# Note:
-#   Sort input source files if you glob sources to ensure bit-for-bit
-#   reproducible builds (https://github.com/pybind/python_example/pull/53)
 
+class get_pybind_include(object):
+    """Helper class to determine the pybind11 include path
+    The purpose of this class is to postpone importing pybind11
+    until it is actually installed, so that the ``get_include()``
+    method can be invoked."""
+
+    def __init__(self, user=False):
+        self.user = user
+
+    def __str__(self):
+        import pybind11
+
+        return pybind11.get_include(self.user)
+
+
+basedir = os.path.dirname(__file__)
+include_dir_user = os.path.join(basedir, "include")
 ext_modules = [
-    Pybind11Extension(
+    Extension(
         "invind",
-        glob("src/*.cpp"),
-        # Example: passing in the version to the compiled code
-        define_macros=[("VERSION_INFO", __version__)],
-        include_dirs=["include"],
-        cxx_std="11",
-    )
+        [
+            "src/field.cpp",
+            "src/indexer.cpp",
+            "src/query.cpp",
+            "src/wrapper.cpp",
+        ],
+        include_dirs=[
+            # Path to pybind11 headers
+            get_pybind_include(),
+            get_pybind_include(user=True),
+            include_dir_user,
+        ],
+        language="c++",
+    ),
 ]
 
-install_requires = [
-    "pybind11>=2.6.0",
-    "numpy >= 1.11",
-]
+
+# As of Python 3.6, CCompiler has a `has_flag` method.
+# cf http://bugs.python.org/issue26689
+def has_flag(compiler, flagname):
+    """Return a boolean indicating whether a flag name is supported on
+    the specified compiler.
+    """
+    import tempfile
+
+    with tempfile.NamedTemporaryFile("w", suffix=".cpp") as f:
+        f.write("int main (int argc, char **argv) { return 0; }")
+        try:
+            compiler.compile([f.name], extra_postargs=[flagname])
+        except setuptools.distutils.errors.CompileError:
+            return False
+    return True
+
+
+def cpp_flag(compiler):
+    """Return the -std=c++[11/14/17] compiler flag.
+    The newer version is prefered over c++11 (when it is available).
+    """
+    flags = ["-std=c++11"]
+
+    for flag in flags:
+        if has_flag(compiler, flag):
+            return flag
+
+    raise RuntimeError("Unsupported compiler -- at least C++11 support " "is needed!")
+
+
+class BuildExt(build_ext):
+    """A custom build extension for adding compiler-specific options."""
+
+    c_opts = {
+        "msvc": ["/EHsc"],
+        "unix": ["-march=native"],
+    }
+    l_opts = {
+        "msvc": [],
+        "unix": [],
+    }
+
+    if sys.platform == "darwin":
+        darwin_opts = ["-stdlib=libc++", "-mmacosx-version-min=10.7"]
+        c_opts["unix"] += darwin_opts
+        l_opts["unix"] += darwin_opts
+
+    def build_extensions(self):
+        ct = self.compiler.compiler_type
+        opts = self.c_opts.get(ct, [])
+        link_opts = self.l_opts.get(ct, [])
+        if ct == "unix":
+            opts.append('-DVERSION_INFO="%s"' % self.distribution.get_version())
+            opts.append(cpp_flag(self.compiler))
+            if has_flag(self.compiler, "-fvisibility=hidden"):
+                opts.append("-fvisibility=hidden")
+        elif ct == "msvc":
+            opts.append('/DVERSION_INFO=\\"%s\\"' % self.distribution.get_version())
+        for ext in self.extensions:
+            ext.extra_compile_args = opts
+            ext.extra_link_args = link_opts
+        build_ext.build_extensions(self)
 
 
 setup(
     name="invind",
     version=__version__,
     author="Tomoki Ohtsuki",
-    author_email="tomoki.ohtsuki.19937@outlook.jp",
-    description="",
+    author_email="tomoki.ohtsuki129@gmail.com",
+    description="Yet another CGS sampler for Latent Dirichlet Allocation.",
     long_description="",
     ext_modules=ext_modules,
-    extras_require={"test": "pytest"},
-    setup_requires=install_requires,
     install_requires=install_requires,
-    cmdclass={"build_ext": build_ext},
+    setup_requires=install_requires,
+    cmdclass={"build_ext": BuildExt},
+    packages=[],
     zip_safe=False,
 )
--- tests/test.py
+++ tests/test.py
@@ -1,11 +1,12 @@
-import sys
 from invind import Indexer
+import pandas as pd
 
 indexer = Indexer()
-indexer.add_categorical_field("hoge").add_categorical_field("job")
-
-indexer.add_index(dict(hoge="1", job="J01"))
-indexer.add_index(dict(hoge="1", job="J02"))
-indexer.add_index(dict(hoge="2", job="J01"))
-
-print(indexer.query_execute_batch([dict(hoge="1"), dict(hoge=["1", "2"])], 3))
+indexer.add_many_to_many_field("job_category_names")
+sample = pd.read_csv("./sample_jcd.csv")
+for row in sample.itertuples():
+    indexer.add_index(dict(job_category_names=row.job_category_names.split("__")))
+qres = indexer.query_execute_batch(
+    [dict(job_category_names=dict(contains_all=["法人営業", "営業支援・プリセールス"]))], 3
+)
+print(sample.iloc[qres[0]])
